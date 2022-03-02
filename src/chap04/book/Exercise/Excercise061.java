ef20e28efb39463de110d97038e65e9cb24514d2
Attmoon <gus89580@naver.com>
2022-03-02 14:05:09 +0900

    Update Excercise061.java

--- src/chap04/book/Exercise/Excercise061.java
+++ src/chap04/book/Exercise/Excercise061.java
@@ -49,7 +49,7 @@ public class Excercise061 {
 		// *****
 		for (int i = 0; i < 5; i++) {
 			for (int k = 0; k < (4 - i); k++) {
-				System.out.println(" ");
+				System.out.print(" ");
 			}
 			for (int j = 0; j < (i + 1); j++) {
 				System.out.print("*");
