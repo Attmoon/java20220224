39365d3e9d07858b73eed8e3036657acb1f661aa
Attmoon <gus89580@naver.com>
2022-03-11 14:59:19 +0900

    java

---
+++ src/chap06/book/exercise/p16/Printer.java
@@ -0,0 +1,19 @@
+package chap06.book.exercise.p16;
+
+public class Printer {
+	void println(int i) {
+		System.out.println(i);
+	}
+	
+	void println(boolean b) {
+		System.out.println(b);
+	}
+	
+	void println(double c) {
+		System.out.println(c);
+	}
+	
+	void println(String d) {
+		System.out.println(d);
+	}
+}
