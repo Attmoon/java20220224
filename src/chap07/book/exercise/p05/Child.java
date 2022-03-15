6935b0ef0dbb7e718fbc702110b0f4f9e92b5b3c
Attmoon <gus89580@naver.com>
2022-03-15 14:07:43 +0900

    java

---
+++ src/chap07/book/exercise/p05/Child.java
@@ -0,0 +1,10 @@
+package chap07.book.exercise.p05;
+
+public class Child extends Parent {
+	private int studentNo;
+	
+	public Child(String name, int studentNo) {
+		this.name = name;
+		this.studentNo = studentNo;
+	}
+}
