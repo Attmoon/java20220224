ca41a54886b47a8b1e8dd8a229a1e317992e8a90
Attmoon <gus89580@naver.com>
2022-03-17 14:59:50 +0900

    java

--- src/chap08/lecture/p01interface/example_class/Cat.java
+++ src/chap08/lecture/p01interface/example_class/Cat.java
@@ -11,5 +11,10 @@ public class Cat extends Feline implements Pet {
 	public void roll() {
 		System.out.println("고양이가 무시합니다.");
 	}
-
+	
+	// interface의 default method도 재정의가 가능
+	@Override
+	public void sit() {
+		System.out.println("고양이가 도망갑니다.");
+	}
 }
