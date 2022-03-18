29420c58d0a5375e680d78f85fb41ec2c021daf1
Attmoon <99157555+Attmoon@users.noreply.github.com>
2022-03-18 17:46:25 +0900

    java

---
+++ src/chap09/book/s090502/Button.java
@@ -0,0 +1,17 @@
+package chap09.book.s090502;
+
+public class Button {
+	OnClickListener listener;
+	
+	void setOnclickListener(OnClickListener listener) {
+		this.listener = listener;
+	}
+	
+	void touch() {
+		listener.onClick();
+	}
+	
+	interface OnClickListener {
+		void onClick();
+	}
+}
