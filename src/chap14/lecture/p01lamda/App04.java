2cc8933634c516454413b6246db683a535388347
Attmoon <gus89580@naver.com>
2022-03-25 10:01:51 +0900

    lamda.java

---
+++ src/chap14/lecture/p01lamda/App04.java
@@ -0,0 +1,22 @@
+package chap14.lecture.p01lamda;
+
+public class App04 {
+
+}
+
+// functional interface : 추상 메소드가 1개인 인터페이스
+@FunctionalInterface // functional interface인지 검수해줌
+interface MyInterface41 {
+	void method1();
+}
+
+//@FunctionalInterface
+interface MyInterface42 {
+	void method2();
+	void method3();
+}
+
+//@FunctionalInterface
+interface MyInterface43 {
+	
+}
